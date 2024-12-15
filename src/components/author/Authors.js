169a9224cc98e8c27c55326a0652a662e34af07c
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2024-12-15 17:07:51 +0330

    Catch Authors Data

--- src/components/author/Authors.js
+++ src/components/author/Authors.js
@@ -1,6 +1,16 @@
+import { useQuery } from "@apollo/client";
 import React from "react";
+import { GET_AUTHORS_INFO } from "../../graphql/queries";
 
 function Authors() {
+  const { loading, data, errors } = useQuery(GET_AUTHORS_INFO);
+
+  if (loading) return <h3>Loading...</h3>;
+
+  if (errors) return <h3>Error...</h3>;
+
+  console.log(data);
+
   return <div>Authors</div>;
 }
 
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,23 +1,35 @@
 import { gql } from "@apollo/client";
 
 const GET_BLOGS_INFO = gql`
-query{
-  posts {
-    title
-    slug
-    id
-    coverPhoto {
-      url
+  query {
+    posts {
+      title
+      slug
+      id
+      coverPhoto {
+        url
+      }
+    }
+    authors {
+      name
+      avatar {
+        url
+      }
     }
   }
-  authors {
-    name
-    avatar {
-      url
-    }
-  }
-}
 `;
 
-export { GET_BLOGS_INFO };
+const GET_AUTHORS_INFO = gql`
+  query {
+    authors {
+      id
+      name
+      slug
+      avatar {
+        url
+      }
+    }
+  }
+`;
 
+export { GET_BLOGS_INFO ,GET_AUTHORS_INFO};
