2b91f2069187e0614eba1bcc8bacbe19951d0aa1
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2024-12-16 14:13:10 +0330

    Dynamically create author details page

--- src/components/author/AuthorsPage.js
+++ src/components/author/AuthorsPage.js
@@ -2,6 +2,7 @@ import { useQuery } from "@apollo/client";
 import React from "react";
 import { useParams } from "react-router-dom";
 import { GET_AUTHOR_INFO } from "../../graphql/queries";
+import { Avatar, Container, Grid, Typography } from "@mui/material";
 
 function AuthorsPage() {
   const { slug } = useParams();
@@ -14,9 +15,34 @@ function AuthorsPage() {
 
   if (errors) return <h3>Error...</h3>;
 
-  console.log(data)
+  console.log(data);
 
-  return <div>AuthorsPage</div>;
+  const { author } = data;
+
+  return (
+    <Container maxWidth="lg">
+      <Grid container mt={10}>
+        <Grid
+          item
+          xs={12}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+        >
+          <Avatar src={author.avatar.url} sx={{ width: 250, height: 250 }} />
+          <Typography component="h3" variant="h5" fontWeight={700} mt={4}>
+            {data.author.name}
+          </Typography>
+          <Typography component="p" variant="h5" color="text.secondary" mt={2}>
+            {author.field}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          {author.description.html}
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default AuthorsPage;
